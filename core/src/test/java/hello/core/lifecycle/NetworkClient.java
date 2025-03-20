bdcd7d73b6d3efbf7a66c6ec77040c7539b7157f
Alex-MJY <jmoon0227@gmail.com>
2025-03-20 18:33:34 +0900

    ANNOTATION @PostConstruct, @PreDestroy

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,7 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;import org.springframework.beans.factory.InitializingBean;
 
 public class NetworkClient {
@@ -28,12 +30,14 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close(){
         System.out.println("NetworkClient.close");
         disconnect();
