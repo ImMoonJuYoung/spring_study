d768ae7c7b823e5b4f5e77752c8ed08231be2050
Alex-MJY <jmoon0227@gmail.com>
2025-05-02 13:20:15 +0900

    ITEM ADD PROCESS - @ModelAttribute

--- item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,9 +36,41 @@ public class BasicItemController {
         return "/basic/addForm";
     }
 
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model) {
+        // 변수를 하나하나 받아서 처리하기 불편함
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) { // view에 자동으로 넣어줌. model.addAttribute 필요 없음.
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // 자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        // 어노테이션에 파라미터를 넣지 않으면 클래스 이름의 첫글자를 소문자로 취급해서 뷰 페이지로 넘긴다.
+        // Item -> item
+
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
     @PostMapping("/add")
-    public String save() {
-        return "xxx";
+    public String addItemV4(Item item) { // 최대한으로 생략 가능. 가독성은 별로임.
+        itemRepository.save(item);
+        return "basic/item";
     }
 
 
