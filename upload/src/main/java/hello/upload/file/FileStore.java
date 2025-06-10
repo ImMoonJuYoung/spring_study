a32a97b7c799a4e203a3cf7c93e12d49b26ef034
Alex-MJY <jmoon0227@gmail.com>
2025-06-10 18:08:17 +0900

    Store Files

--- upload/src/main/java/hello/upload/file/FileStore.java
+++ upload/src/main/java/hello/upload/file/FileStore.java
@@ -7,6 +7,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -17,6 +19,15 @@ public class FileStore {
 
     public String getFullPath(String filename) {
         return fileDir + filename;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
     }
 
     public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
@@ -30,7 +41,7 @@ public class FileStore {
 
         // 파일 저장
         multipartFile.transferTo(new File(getFullPath(storeFileName)));
-        
+
         return new UploadFile(originalFilename, storeFileName);
     }
 
