f52fdbddf730af466ace629183b29e5f721aea1c
Alex-MJY <jmoon0227@gmail.com>
2025-07-10 15:39:16 +0900

    Transaction Propagation Utilization - Commit, Rollback

--- spring-db2/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ spring-db2/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,10 +1,12 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
@@ -31,6 +33,20 @@ class MemberServiceTest {
         // then
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
+    }
+
+    @Test
+    void outerTxOff_fail() {
+        // given
+        String username = "로그예외_outerTxOff_fail";
+
+        // when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        // then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
     }
 
 }
