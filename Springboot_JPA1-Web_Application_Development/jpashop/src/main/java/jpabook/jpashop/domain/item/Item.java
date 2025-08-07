d9214f24f009d8baf0783b1fd2b7787b6b18b163
Alex-MJY <jmoon0227@gmail.com>
2025-08-07 13:24:04 +0900

    JPA Shop - Item Entity (add business logic)

--- Springboot_JPA1-Web_Application_Development/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ Springboot_JPA1-Web_Application_Development/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockExcepction;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +26,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) { // 재고 증가
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) { // 재고 감소
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
