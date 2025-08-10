fea6c3bbc72ecb2b36d54ee3daf0d681613ddafb
Alex-MJY <jmoon0227@gmail.com>
2025-08-10 20:03:15 +0900

    JPA Shop - Web (Item Modified)

--- Springboot_JPA1-Web_Application_Development/jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
+++ Springboot_JPA1-Web_Application_Development/jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter @Setter
 public class BookForm {
 
-    private String id;
+    private Long id;
     private String name;
     private int price;
     private int stockQuantity;
