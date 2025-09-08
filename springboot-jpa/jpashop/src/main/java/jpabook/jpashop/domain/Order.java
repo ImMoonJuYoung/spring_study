4bb89096d678044abf3061ab127e1bcf0bb39c73
Alex-MJY <jmoon0227@gmail.com>
2025-09-08 22:13:50 +0900

    JPA2 -  주문 조회 V2: 엔티티를 DTO로 변환- 페이징과 한계 돌파

--- springboot-jpa/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ springboot-jpa/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 import org.thymeleaf.standard.expression.Each;
 
 import java.time.LocalDateTime;
@@ -25,6 +26,7 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member; // 주문 회원
 
+//    @BatchSize(size = 1000)
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
