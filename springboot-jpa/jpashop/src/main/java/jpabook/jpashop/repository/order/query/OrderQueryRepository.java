55269272210b513226aba2bf87955c3f0149a754
Alex-MJY <jmoon0227@gmail.com>
2025-09-09 16:02:37 +0900

    JPA2 -  주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- springboot-jpa/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ springboot-jpa/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -46,7 +46,7 @@ public class OrderQueryRepository {
                 .getResultList();
 
         Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
-                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));  
+                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
 
         result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
 
@@ -63,4 +63,14 @@ public class OrderQueryRepository {
     }
 
 
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class
+        ).getResultList();
+    }
 }
