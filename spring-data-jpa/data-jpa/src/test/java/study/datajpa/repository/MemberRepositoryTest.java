2b310a3101a26c2a4aabe63dba21143ea850f2f6
Alex-MJY <jmoon0227@gmail.com>
2025-09-12 15:00:55 +0900

    Spring Data JPA - 반환타입

--- spring-data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -126,6 +127,21 @@ class MemberRepositoryTest {
             System.out.println("member = " + member);
         }
     }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+//        List<Member> aaa = memberRepository.findListByUsername("AAA");
+
+        Optional<Member> result = memberRepository.findOptionalByUsername("asdfwef");
+        System.out.println("result = " + result);
+    }
+
+
 
 
 }
