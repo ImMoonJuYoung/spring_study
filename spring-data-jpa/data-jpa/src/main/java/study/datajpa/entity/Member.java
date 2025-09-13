f4c621d71b15b032983a6b99f74feb37e17339e6
Alex-MJY <jmoon0227@gmail.com>
2025-09-13 14:47:58 +0900

    Spring Data JPA - Auditing (created, modified)

--- spring-data-jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ spring-data-jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -8,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
 @NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
-public class Member {
+public class Member extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
